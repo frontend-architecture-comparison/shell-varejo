472f3430921549709b2bcb7c60d2aa6bd25b5a3c
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-04-16 19:46:45 -0300

    feat: add listagem dentro do shell + mock server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'shell-varejo';
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly router = inject(Router);
+  private currentUrl = this.router.url;
+  private routeSubscription?: Subscription;
+  protected cartQuantidade = 0;
+
+  protected carrinhoState = {
+    quantidade: () => this.cartQuantidade,
+  };
+
+  protected get title(): string {
+    const url = this.currentUrl;
+
+    switch (true) {
+      case url === '/':
+      case url.startsWith('/home'):
+        return 'Home';
+      case url.startsWith('/carrinho'):
+        return 'Carrinho';
+      default:
+        return '';
+    }
+  }
+
+  protected get isCarrinhoRoute(): boolean {
+    return this.currentUrl.startsWith('/carrinho');
+  }
+
+  ngOnInit(): void {
+    this.routeSubscription = this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+        map(() => this.router.url),
+      )
+      .subscribe((url) => {
+        this.currentUrl = url;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+  }
+
+  goToCart(): void {
+    this.router.navigate(['/carrinho']);
+  }
+
+  goToHome(): void {
+    this.router.navigate(['/home']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/components/header/header';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
